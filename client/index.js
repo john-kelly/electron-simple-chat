d214a0893f61bb1fe968e113017398d1b4253bcb
john kelly <johnkelly@codehs.com>
2015-06-23 10:30:50 -0700

    proto login

--- client/index.js
+++ client/index.js
@@ -1,12 +1,32 @@
 'use strict';
 
+// 3rd Party.
 var io = require('socket.io-client');
 
 // This file is placed into ./build during build.
 var env = require('./env_config.json');
 
-var socket = io(env.serverUrl);
+// CONSTANTS
+var LOGIN_BUTTON = 'login-btn';
+var USERNAME_INPUT = 'username-input';
 
+var socket = io(env.serverUrl);
 socket.on('connect', function() {
     console.log('You have connected.');
+});
+
+socket.on('login:status', function(data) {
+    if (data.status === 'ok') {
+        console.log('login ok');
+    } else {
+        console.log('login failed');
+    }
+});
+
+// Login button setup.
+var loginButton = document.getElementsByClassName(LOGIN_BUTTON)[0];
+loginButton.addEventListener('click', function() {
+    var usernameInput = document.getElementsByClassName(USERNAME_INPUT)[0];
+    var username = usernameInput.value;
+    socket.emit('login', {username:username});
 });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var io = require('socket.io');
+var _ = require('underscore');
 
 var app = express();
 var server = http.Server(app);
@@ -8,8 +9,22 @@ var webSocketServer = io(server);
 
 app.set('port', (process.env.PORT || 5000));
 
-webSocketServer.on('connection', function(socket) {
-    console.log('a user connected');
+// Global usernames of users in chatroom.
+var usernames = [];
+
+webSocketServer.on('connection', function(client) {
+    console.log('a client has connected');
+
+    client.on('login', function(data) {
+        var username = data.username;
+
+        if (_.contains(usernames, username)) {
+            client.emit('login:status', {status: 'fail'});
+        } else {
+            usernames.push(username);
+            client.emit('login:status', {status: 'ok'});
+        }
+    });
 });
 
 server.listen(app.get('port'), function() {
